238fb29ed9ba51d33582ad2b7633aa9e06aeec02
lazydaisy <lazydaisy@visible-expression.co.uk>
2017-04-03 03:19:42 +0100

    Seconed commit

--- lang/en/theme_inspiration.php
+++ lang/en/theme_inspiration.php
@@ -15,25 +15,29 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Language file.
- *
- * @package   theme_boost
- * @copyright 2016 Frédéric Massart
+ * @package   theme_inspiration
+ * @copyright 2017 byLazyDaisy.uk
+ * @date      2nd April 2017
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
 
 defined('MOODLE_INTERNAL') || die();
 
 $string['advancedsettings'] = 'Advanced settings';
+
 $string['brandcolor'] = 'Brand colour';
 $string['brandcolor_desc'] = 'The accent colour.';
-$string['choosereadme'] = 'Boost is a modern highly-customisable theme. This theme is intended to be used directly, or as a parent theme when creating new themes utilising Bootstrap 4.';
+
+$string['choosereadme'] = 'Inspiration is a modern highly-customisable theme. This theme is intended to be used directly, or as a parent theme when creating new themes utilising Bootstrap 4.';
 $string['currentinparentheses'] = '(current)';
-$string['configtitle'] = 'Boost';
+$string['configtitle'] = 'Inspiration';
+
 $string['generalsettings'] = 'General settings';
-$string['pluginname'] = 'Boost';
+
+$string['pluginname'] = 'Inspiration';
 $string['presetfiles'] = 'Additional theme preset files';
-$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Inspiration_Presets>Inspiration presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
 $string['preset'] = 'Theme preset';
 $string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
 $string['rawscss'] = 'Raw SCSS';
